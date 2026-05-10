4ebb274b1f5bef69a87b8fc718b8f93937a0f2c1
Phil Surette <phil.surette@gmail.com>
2026-05-10 16:11:06 -0400

    Install Cuemaster dependencies and fix test config

--- cuemaster/vite.config.ts
+++ cuemaster/vite.config.ts
@@ -1,10 +1,11 @@
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   plugins: [react()],
   test: {
     environment: "jsdom",
+    include: ["tests/unit/**/*.test.ts"],
     setupFiles: ["tests/unit/setup.ts"]
   }
 });
